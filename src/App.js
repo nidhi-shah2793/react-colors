4dcf2c864e16ced053b99e1f46d371d958406053
Nidhi <nidhi.shah2793@gmail.com>
2020-12-16 15:59:42 -0500

    Added unstyled slider and color levels

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from "react";
 import "./App.css";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
+import { generatePalette } from "./colorHelpers";
 
 function App() {
   return (
     <div className="App">
-      <Palette {...seedColors[4]} />
+      <Palette {...seedColors[4]} palette={generatePalette(seedColors[4])} />
     </div>
   );
 }
--- src/Palette.js
+++ src/Palette.js
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import ColorBox from "./ColorBox";
 import "./Palette.css";
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
 
-function Palette({ paletteName, id, emoji, colors }) {
-  const colorBoxes = colors.map((color) => (
-    <ColorBox color={color.color} name={color.name} />
+function Palette({ palette }) {
+  const [level, setLevel] = useState(500);
+
+  const colorBoxes = palette.colors[level].map((color) => (
+    <ColorBox key={color.id} color={color.hex} name={color.name} />
   ));
+
+  const changeLevel = (newLevel) => {
+    setLevel(newLevel);
+  };
 
   return (
     <div className="Palette">
       {/* Navbar goes here */}
+      <Slider
+        defaultValue={level}
+        step={100}
+        min={100}
+        max={900}
+        onAfterChange={changeLevel}
+      />
       <div className="Palette__colors">{colorBoxes}</div>
       {/* footer */}
     </div>
